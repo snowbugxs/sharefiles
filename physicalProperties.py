99962086a2d0258a65c190beef004cd299e5f850
snowbugxs <54727599+snowbugxs@users.noreply.github.com>
2019-09-16 20:00:25 -0400

    physical_properties_NiTi

---
+++ physicalProperties.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 12:52:18 2019
+
+@author: local_admin
+"""
+
+### powder information from \cite{Yablokova2015} ### 
+### NiTi ### 
+## density kg/m3 ## 
+denNiTi = 6470 
+## modulus 
+yonmod = 84e9 
+## posion ratio ## 
+psnrio = 0.3 
+## size distribution 45-100 um ## 
+dsize = [42.61e-6, 43.04e-6, 67.97e-6, 103.57e-6, 104.61e-6], [0, 0.1, 0.5, 0.9, 1.0] 
+
+## cohesive index: 6rpm~29~9.8 ##  
+
+## NiTi ends ## 
+
+
+### material 2  ###
+
+
+
+
+
+### material 2 ens ###  
